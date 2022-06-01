1f4e37e31754bbfc1e8e6333a544a1730e24792e
Louis Y <louisy603@hotmail.com>
2022-06-01 16:45:25 -0400

    added params filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,25 +11,34 @@ class ApplicationController < Sinatra::Base
   get "/pokemons" do
     pokemons = Pokemon.all
     # pokemons.to_json(include:{moves: { include: [:type]}})
-    pokemons.to_json(include: :type)
+    pokemons.to_json
+    # (include: :type)
   end
 
   get '/pokemons/:id' do 
     pokemons = Pokemon.find(params[:id])
-    pokemons.to_json(include:{moves: {include: [:type]}})
+    pokemons.to_json(include:{moves: {include: [:type]}}, methods: [:types])
   end
 
   post '/pokemons' do
-    pokemons = Pokemon.create(
-      name: params[:name],
-      hp: params[:hp],
-      height: params[:height],
-      met_at: params[:met_at],
-      description: params[:description],
-      type_id: params[:type_id],
-      image: params[:image]
-    )
-    pokemons.to_json
+    Pokemon.create(pokemon_params).to_json
+    # (
+    #   name: params[:name],
+    #   hp: params[:hp],
+    #   height: params[:height],
+    #   met_at: params[:met_at],
+    #   description: params[:description],
+    #   type_id: params[:type_id],
+    #   image: params[:image]
+    # )
+    # pokemons.to_json
+  end
+
+  def pokemon_params
+    allowed_params = [ "name", "hp", "height", "met_at", "description", "type_id", "image"]
+    params.filter do |param, value|
+      allowed_params.include?(param)
+    end
   end
 
   delete '/pokemons/:id' do 
@@ -37,8 +46,14 @@ class ApplicationController < Sinatra::Base
     pokemons.to_json
   end
 
+  delete '/pokemon_moves/:id' do
+    pokemons = PokemonMove.where(pokemon_id: params[:id]).destroy_all
+    pokemons.to_json
+  end
+
   patch '/pokemons/:id' do 
-    pokemons = Pokemon.find(params[:id]).update(
+    pokemons = Pokemon.find(params[:id])
+    pokemons.update(
       name: params[:name],
       hp: params[:hp],
       height: params[:height],
